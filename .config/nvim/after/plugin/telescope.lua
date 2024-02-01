3474189ec8d3964d2bb33f005e2fd43dd9dcd304
James Plummer <plum_1990@hotmail.co.uk>
2024-02-01 19:01:40 +0000

    update telescope keymaps

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -6,20 +6,13 @@ require('telescope').load_extension('fzf')
 vim.keymap.set('n', '<leader><F1>', builtin.help_tags, { desc = '[S]earch [H]elp'})
 
 vim.keymap.set('n', '<leader>fp', builtin.oldfiles, { desc = '[F]ind [P]reviously opened files' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind existing [B]uffers' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
-
-vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Find in Files' })
-vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = '[F]ind in [G]it Files' })
+vim.keymap.set('n', '<leader>fo', builtin.buffers, { desc = '[F]ind [O]pen buffers' })
+vim.keymap.set('n', '<leader>f/', builtin.current_buffer_fuzzy_find, { desc = '[f/] Fuzzily search in current buffer' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind in [F]iles' })
 vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
-vim.keymap.set('n', '<leader>gf', builtin.live_grep, { desc = '[G]rep [F]ind' })
-vim.keymap.set('n', '<leader>gF', ':LiveGrepGitRoot<cr>', { desc = '[G]rep [F]ind on Git Root' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind with [G]rep' })
 vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind in [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
+vim.keymap.set('n', '<leader>fb', builtin.git_branches, { desc = '[F]ind git [B]ranches' })
+vim.keymap.set('n', '<leader>fc', builtin.git_bcommits, { desc = '[F]ind git [C]commits for current buffer' })
 
